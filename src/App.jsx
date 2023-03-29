701c75175341cca7a797929a9447e0940153cfee
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-03-28 20:37:19 -0700

    partial

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from './Components/Header';
 import Todo from './Components/Todo';
 import Footer from './Components/Footer';
+import Settings from './Components/Settings';
+import { Routes, Route } from '@react-router-dom';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-      <Header />
-      <Todo />
-      <Footer />
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<Todo/>} />
+          <Route path='/settings' element={<Settings/>}/>
+        </Routes> 
+        <Footer />
       </>
     );
   }
